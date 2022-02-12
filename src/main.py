4ab793d251bd645f6be41454bc517620785e7822
ManuuMT <ema14mt@gmail.com>
2022-02-12 19:57:54 +0000

    First model and endpoint done

--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -31,13 +30,18 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_users():
+    # get all the user
+    user_query = User.query.all()
+    # map the results and your list of user inside of the all_user variable
+    all_user = list(map(lambda x: x.serialize(), user_query))
+    return jsonify(all_user), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_just_one_user(user_id):
+    user = User.query.get(user_id)
+    return jsonify(user.serialize()), 200
+    
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,19 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    #favorite_planets = db.relationship("FavoritePlanet", backref="planet_faver")
+    #favorite_characters = db.relationship("FavoriteCharacter", backref="char_faver")
+    
     def __repr__(self):
         return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "username": self.username,
+            "active": self.is_active, 
             # do not serialize the password, its a security breach
         }
